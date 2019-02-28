79baae27318d788bb4dfce6320bb7c231ae2a09f
FWO <wojciech.fijarczyk@hexagon.com>
2019-02-28 09:54:21 +0100

    added idea files to ignored files

--- tcpserver.py
+++ tcpserver.py
@@ -6,25 +6,26 @@ bind_port = 9999
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind((bind_ip,bind_port))
+server.bind((bind_ip, bind_port))
 
-#arg - max number of requests waiting on backlog queue
+# arg - max number of requests waiting on backlog queue
 #
 server.listen(5)
 
 print "[] Listening on %s:%d" % (bind_ip, bind_port)
 
-#Thread to handle client connections
+
+# Thread to handle client connections
 def handle_client(client_socket):
 
     request = client_socket.recv(1024)
-
 
     print "[] Received: %s" % request
 
     client_socket.send("ACK!")
 
     client_socket.close()
+
 
 while True:
     client, address = server.accept()
